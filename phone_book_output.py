a04cc6bb77269ac7d541205024c8ce46b1cf4e8b
Vladimir Rozenberg <vvr_prim@mail.ru>
2022-11-21 11:55:20 +1000

    добавил файл phone_book_output.py

---
+++ phone_book_output.py
@@ -0,0 +1,22 @@
+import sqlite3 as sql
+
+global conn
+global cursor
+
+conn = sql.connect('data_users.db')
+cursor = conn.cursor()
+
+def abonent_output():
+    print('Введите данные абонента:\n')
+    surname_output = input('Фамилия: ')
+    name_output = input('Имя: ')
+    patron_output = input('Отчество: ')
+    print()
+    
+    cursor.execute(f"SELECT phone_number FROM phone_list WHERE (surname, name, patron) = ('{surname_output}', '{name_output}', '{patron_output}')")
+    result = cursor.fetchall()
+    if result == []:
+        print('Данный абонент отсутствует в справочнике!')
+    else:
+        for row in result:
+            print(f'Номер телефона: {row[0]}')
